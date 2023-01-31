c4baa289fa40ec36314efa59c7b10f146c8dada0
Tommas138 <tomaso200208@gmail.com>
2023-01-31 00:37:40 -0300

    Agrego javascript

---
+++ java.js
@@ -0,0 +1,25 @@
+
+var buttonText = document.getElementById('button-text')
+buttonText.addEventListener("click", display)
+
+function display() {
+
+    document.getElementById("mostrar").style.display = 'block';
+}
+
+var buttonText2 = document.getElementById('button-text2')
+buttonText2.addEventListener("click", display2)
+
+function display2() {
+
+    document.getElementById("mostrar2").style.display = 'block';
+}
+
+var buttonText3 = document.getElementById('button-text3')
+buttonText3.addEventListener("click", display3)
+
+function display3() {
+
+    document.getElementById("mostrar3").style.display = 'block';
+}
+
